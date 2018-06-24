6a61f45ac669a938bd497ef18dc0682312c845bc
Rajnish Kumar <ravisai.rajnish@gmail.com>
2018-06-24 19:04:38 +0530

    checking emails

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -3,14 +3,44 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
     
-	public function index()
+     public function __construct()
 	{
-        $title['title_of_page'] = "";
-        $title['description'] = "";
-        $title['keywords'] ="";
-        $title['login'] = 1;
-        $this->load->view('include/header', $title);
-		$this->load->view('login');
-        $this->load->view('include/footer');
+		parent::__construct();
+		$this->load->library('session');
+        $this->load->library('email');
+		$this->load->helper(array('form', 'url'));
+		$this->load->model('My_model');	
+		$this->load->model('Employer_model');	
+		$this->load->model('LanguageExpert_model');		
+	}
+    public function index()
+	{
+        $config['protocol'] = 'smtp';
+        $config['charset'] = 'utf-8';
+        $config['wordwrap'] = TRUE;
+        $config['smtp_host'] = "tls://email-smtp.us-east-1.amazonaws.com"; // eg. tls://email-smtp.eu-west-1.amazonaws.com
+        $config['smtp_user'] = "AKIAIOJTHBVQVH6TWPKQ";
+        $config['smtp_pass'] = "AkrowXorCgcGBIpkfOR/l96OrhGzZQIVhJwgNUZrN4Ym";
+        $config['smtp_port'] = "465";
+        $config['smtp_timeout'] = "20";
+        $config['crlf'] = "\r\n";
+        $config['mailsender'] = "admin@langjobs.com";
+        
+        $this->email->initialize($config);
+        $this->email->set_mailtype("html");
+        $this->email->set_newline("\r\n");
+
+        //Email content
+        $htmlContent = '<h1>Sending email via SMTP server</h1>';
+        $htmlContent .= '<p>This email has sent via SMTP server from CodeIgniter application.</p>';
+
+        $this->email->to('rajnish.kumar@langecole.com');
+        $this->email->from('admin@langjobs.com','LangJobs.com');
+        $this->email->subject('Email Successful');
+        $this->email->message($htmlContent);
+
+        //Send email
+        $this->email->send();
+        
 	}
 }
